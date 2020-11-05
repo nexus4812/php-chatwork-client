3ab1210882f93e786e8f24108c916586c1d908c2
nexus4812 <powerbatteryboy@gmail.com>
2020-11-06 08:37:02 +0900

    Add phpdoc

--- src/Entities/Contacts.php
+++ src/Entities/Contacts.php
@@ -6,12 +6,42 @@ namespace ChatWorkClient\Entities;
 
 class Contacts implements EntityInterface
 {
+    /**
+     * @var int
+     */
     public $account_id;
+    /**
+     * @var int
+     */
     public $room_id;
+
+    /**
+     * @var string
+     */
     public $name;
+
+    /**
+     * @var string
+     */
     public $chatwork_id;
+
+    /**
+     * @var int
+     */
     public $organization_id;
+
+    /**
+     * @var string
+     */
     public $organization_name;
+
+    /**
+     * @var string
+     */
     public $department;
+
+    /**
+     * @var string
+     */
     public $avatar_image_url;
 }
