5e813bf8104656e95f4aa5f7628b161b1d8ff15f
nexus4812 <powerbatteryboy@gmail.com>
2020-11-18 08:40:53 +0900

    Delete arrayToCollection

--- src/Api/EndPoints/AbstractEndPoint.php
+++ src/Api/EndPoints/AbstractEndPoint.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Nexus\ChatworkClient\Api\EndPoints;
 
-use Illuminate\Support\Collection;
 use Nexus\ChatworkClient\Client\ClientInterface;
 use Nexus\ChatworkClient\Entities\Factories\EntityFactoryInterface;
 
@@ -24,10 +23,5 @@ abstract class AbstractEndPoint
     {
         $this->client = $client;
         $this->factory = $factory;
-    }
-
-    protected function arrayToCollection(array $array): Collection
-    {
-        return new Collection($array);
     }
 }
